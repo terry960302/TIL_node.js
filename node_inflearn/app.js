9e38910d913f65cf34f9dccbaa836552bc96cc35
Taewan Kim <terry960302@gmail.com>
2020-01-24 15:42:32 +0900

    feat : ajax http test

--- node_inflearn/app.js
+++ node_inflearn/app.js
@@ -8,6 +8,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+app.listen(3000, () => {
+  console.log("시작합니다. 3000");
+});
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/public/main.html");
 });
@@ -24,8 +28,22 @@ app.post("/email_post", function(req, res) {
 
 app.post("/ajax_send_email", function(req, res) {
   console.log(req.body.email);
+  //check validation about input value = select db
+  var responseData = { result: "ok", email: req.body.email };
+  res.json(responseData);
 });
 
-app.listen(3000, () => {
-  console.log("시작합니다. 3000");
+//practice
+app.post("/post_test", (req, res) => {
+  res.render("test2.ejs", { myName: req.body.name, myEmail: req.body.email });
+});
+
+app.post("/ajax_test", (req, res) => {
+  console.log(req.body);
+  var responseData = {
+    success: true,
+    name: req.body.name,
+    email: req.body.email
+  };
+  res.json(responseData);
 });
