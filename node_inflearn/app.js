ad363284598b35cf57efddd6aad64c762e954517
Taewan Kim <terry960302@gmail.com>
2020-01-25 13:48:19 +0900

    feat: passport setting

--- node_inflearn/app.js
+++ node_inflearn/app.js
@@ -2,12 +2,26 @@ const express = require("express");
 var app = express();
 const bodyParser = require("body-parser");
 const router = require("./router/index");
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+const flash = require("connect-flash");
 
 //미들웨어 세팅
 app.use(express.static("public")); // 정적 파일 세팅(주소에 파일이름만 적어도 접근 가능)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); //post데이터 화면에 띄우기 위한 body-parser
 app.set("view engine", "ejs"); // 뷰 엔진(ejs나 pug 등등)
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 //서버 시작
 app.listen(3000, () => {
--- node_inflearn/router/join/index.js
+++ node_inflearn/router/join/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mysql = require("mysql");
 const path = require("path");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 //mysql 데이터베이스 연동
 var connection = mysql.createConnection({
@@ -15,23 +17,46 @@ var connection = mysql.createConnection({
 connection.connect();
 
 router.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "../../public/join.html"));
+  res.render("join.ejs");
 });
 
-router.post("/", (req, res) => {
-  var body = req.body;
-  var email = body.email;
-  var name = body.name;
-  var passwd = body.password;
-
-  //회원 디비에 추가
-  var sql = { email: email, name: name, pw: passwd };
-  var query = connection.query("insert into user set ?", sql, (err, rows) => {
-    if (err) {
-      throw err;
+passport.use(
+  "local-join",
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+      passReqToCallback: true
+    },
+    (req, email, password, done) => {
+      console.log("local-join callback called");
     }
-    console.log("잘했어요 : ", rows.insertId, name);
-    res.render("welcome.ejs", { name: name, id: rows.insertId });
-  });
-});
+  )
+);
+
+router.post(
+  "/",
+  passport.authenticate("local-join", {
+    successRedirect: "/main",
+    failureRedirect: "/join",
+    failureFlash: true
+  })
+);
+
+// router.post("/", (req, res) => {
+//   var body = req.body;
+//   var email = body.email;
+//   var name = body.name;
+//   var passwd = body.password;
+
+//   //회원 디비에 추가
+//   var sql = { email: email, name: name, pw: passwd };
+//   var query = connection.query("insert into user set ?", sql, (err, rows) => {
+//     if (err) {
+//       throw err;
+//     }
+//     console.log("잘했어요 : ", rows.insertId, name);
+//     res.render("welcome.ejs", { name: name, id: rows.insertId });
+//   });
+// });
 module.exports = router;
